b3bb4b3a89f2d89feec7d0e905440df67ceea056
johders <169034808+johders@users.noreply.github.com>
2025-07-05 13:50:29 +0200

    Update manual href

--- src/components/ui/TextArea.jsx
+++ src/components/ui/TextArea.jsx
@@ -8,7 +8,7 @@ function TextArea({ value, onChange, id, error }) {
         <div className={appStyles.errorMessage}>
           {error}{" "}
           <a
-            href="https://github.com/johders/.."
+            href="https://github.com/johders/cron-schedule-editor/blob/main/docs/manual.md"
             target="_blank"
             rel="noopener noreferrer"
           >
